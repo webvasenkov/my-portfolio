5d645f38e699fdd60b31fadec23037e27ae025ed
webvasenkov <webvasenkov@gmail.com>
2021-02-19 23:03:16 +0300

    fix logo

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-scroll';
 import { ReactComponent as Logo } from '../assets/logo.svg';
 import { ReactComponent as BigShoesTorso } from '../assets/big-shoes-torso.svg';
 
 const Hero = () => {
+  const [about, setAbout] = useState(['t1', 't2']);
+  const [description, setDescription] = useState('');
+
+  const delay = (ms = 150) => {
+    return new Promise((resolve, reject) => {
+      setTimeout(resolve, ms);
+    });
+  };
+
+  const typeWriterEffect = async () => {
+    for (let i = 0; i < about.length; i++) {
+      setDescription('');
+      for (let j = 0; j < (!i ? about[i].length + 1 : about[i].length); j++) {
+        await delay();
+        setDescription((descriptionPrev) => {
+          return descriptionPrev + about[i][j];
+        });
+      }
+    }
+  };
+
+  useEffect(() => typeWriterEffect(), []);
+
   return (
     <section className='hero'>
       <div className='container'>
@@ -28,7 +51,7 @@ const Hero = () => {
           <h1 className='hero__title'>
             Hi <br /> my name <span className='thin'>is</span> Denis
           </h1>
-          <p className='hero__description'>I’m a beginner web developer</p>
+          <p className='hero__description'>{description}</p>
           <Link className='hero__button red-button' to='portfolio' smooth>
             View Portfolio
           </Link>
